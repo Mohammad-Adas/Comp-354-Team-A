d5afd776c11859b8703fbca2b0f7ef2e2b83af5e
soroushamdg <soroushmpour@gmail.com>
2024-11-01 15:08:39 -0400

    added comments

--- src/Helpers.java
+++ src/Helpers.java
@@ -1,5 +1,12 @@
 public class Helpers {
 
+    /*
+    Please add your helpers functions here.
+     */
+
+    /*
+    Factorial function
+     */
     public static int factorial(int n){
         if (n==0 || n==1){
             return 1;
@@ -7,4 +14,26 @@ public class Helpers {
             return n*factorial(n-1);
         }
     }
+
+
+    /*
+    Absolute Value function
+     */
+
+
+    /*
+    Exponential Function function
+     */
+
+    /*
+    Natural Logarithm function
+     */
+
+    /*
+    Square Root function
+     */
+
+    /*
+    Trigonometric Functions
+     */
 }
